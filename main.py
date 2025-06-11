4939ae04ce276cb1890ac91fdf9bf8d2b5be2108
Sebastião Venâncio <tiaovenancio2@gmail.com>
2025-06-10 22:33:01 -0300

    [#] Corrige 'acuracia_teto'

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     # Aqui vamos executar o método supervisionado que já existe no método train em nets.py
     # O objetivo é obter a acurácia usando todas as amostras do dataset 
     df = executar_supervisionado_puro(DATSET, SEED, n_epoch=1)
-    acuracia_teto = df['Acurácia Conjunto de Teste']
+    acuracia_teto = df['Acurácia Conjunto de Teste'].iloc[0]
 
     # Em seguida, vamos executar cada estratégia individualmente:
     estrategias = ["LeastConfidence", "MarginSampling", "EntropySampling","LeastConfidenceDropout", "MarginSamplingDropout", "EntropySamplingDropout", "KMeansSampling", "BALDDropout"]
